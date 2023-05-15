d5761a189cbb3c8a62b2642f57ccfb6f64f0ebe3
jfmarkan <jfmarkan@gmail.com>
2023-05-15 15:02:20 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -11,12 +11,21 @@ buyButton.addEventListener('click',
         if (age < 18){
             finalPrice = travelPrice - (travelPrice * (19.4 / 100));
             console.log(finalPrice);
+            document.getElementById('travel').innerHTML = travelPrice;
+            document.getElementById('discount').innerHTML = '19.4%';
+            document.getElementById('total').innerHTML = finalPrice;
         } else if (age >= 65) {
             finalPrice = travelPrice - (travelPrice * (37.7 / 100));
             console.log(finalPrice);
+            document.getElementById('travel').innerHTML = travelPrice;
+            document.getElementById('discount').innerHTML = '37.7%';
+            document.getElementById('total').innerHTML = finalPrice;
         } else {
             finalPrice = travelPrice;
             console.log(finalPrice);
+            document.getElementById('travel').innerHTML = travelPrice;
+            document.getElementById('discount').innerHTML = '0%';
+            document.getElementById('total').innerHTML = finalPrice;
         }
     }
     

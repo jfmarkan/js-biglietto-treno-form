93201b0dfaa717c5007f45270f5ce2749c184c5c
jfmarkan <jfmarkan@gmail.com>
2023-05-15 14:38:51 +0200

    Milestone 1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const age = parseInt(document.getElementById('age').value);
+const kilometers = parseInt(document.getElementById('kilometers').value); 
+
+let travelPrice = kilometers * 0.233;
+let finalPrice;
+
+document.addEventListener('click', 
+    function(){
+        if (age < 18){
+            finalPrice = travelPrice - (travelPrice * (19.4 / 100));
+            console.log(finalPrice);
+        } else if (age >= 65) {
+            finalPrice = travelPrice - (travelPrice * (37.7 / 100));
+            console.log(finalPrice);
+        } else {
+            finalPrice = travelPrice;
+            console.log(finalPrice);
+        }
+    }
+    
+)
+
